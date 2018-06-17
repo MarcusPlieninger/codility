f238787d1529367c786897bed5f3f3a0b4a26ef3
MarcusPlieninger <mwp2101@columbia.edu>
2018-06-17 10:52:46 -0400

    fixed with a little math trick

--- ch5_CountDiv.py
+++ ch5_CountDiv.py
@@ -1,20 +1,6 @@
 def solution(A, B, K):
-    if K > A:
-        A = K
-
-    while B % K !=0:
-        B -=1
- 
-    for i in range(A, B+1):
-        A += A+i
-
-    divisiblecount = 0
-    while A % K == 0:
-        A /=K
-        divisiblecount +=1
-
-    return divisiblecount
-
-
-
-
+    result = B // K - A // K
+    if A % K == 0:
+        result += 1
+    
+    return result
