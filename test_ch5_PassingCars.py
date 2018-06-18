9fd4fc4bd28a4b9645135e6fb41cc6e7c203ec24
MarcusPlieninger <mwp2101@columbia.edu>
2018-06-18 05:44:39 -0400

    fixed test

--- test_ch5_PassingCars.py
+++ test_ch5_PassingCars.py
@@ -4,7 +4,7 @@ from ch5_PassingCars import solution
 class example_tests(unittest.TestCase):
     def test_example1(self):
         A = [0, 1, 0, 1, 1]
-        self.assertEqual(solution(A), [(0,1), (0,3), (0,4), (2,3), (2,4)], msg= "example test")
+        self.assertEqual(solution(A), 5, msg= "example test")
 
 class correctness_tests(self):
     def test.one0(self):
