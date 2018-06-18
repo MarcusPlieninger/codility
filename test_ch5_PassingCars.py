fddda34c7b07b4fa75f8b63bbeb72f889ad28553
MarcusPlieninger <mwp2101@columbia.edu>
2018-06-18 05:00:46 -0400

    added a couple basic tests

--- test_ch5_PassingCars.py
+++ test_ch5_PassingCars.py
@@ -6,5 +6,14 @@ class example_tests(unittest.TestCase):
         A = [0, 1, 0, 1, 1]
         self.assertEqual(solution(A), [(0,1), (0,3), (0,4), (2,3), (2,4)], msg= "example test")
 
+class correctness_tests(self):
+    def test.one0(self):
+        A = [1]
+        self.assertEqual(solution(A), 0)
+
+    def test.one1(self):
+        A = [0]
+        self.assertEqual(solution(A), 0)
+
 if __name__ == '__main__':
     unittest.main()
