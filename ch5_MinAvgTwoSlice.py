6c2ed5323f11bfa7fc54c2fb1685264fdd5cfa08
Marcus Plieninger <mwp2101@columbia.edu>
2018-06-21 23:10:58 -0400

    scored 100%

--- ch5_MinAvgTwoSlice.py
+++ ch5_MinAvgTwoSlice.py
@@ -1,71 +1,17 @@
-""" 
-A non-empty array A consisting of N integers is given. A pair of
-integers (P, Q), such that 0 ≤ P < Q < N, is called a slice of array A
-(notice that the slice contains at least two elements). The average of
-a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by
-the length of the slice. To be precise, the average equals 
-(A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
-
-For example, array A such that:
-
-    A[0] = 4
-    A[1] = 2
-    A[2] = 2
-    A[3] = 5
-    A[4] = 1
-    A[5] = 5
-    A[6] = 8
-contains the following example slices:
-
-slice (1, 2), whose average is (2 + 2) / 2 = 2;
-slice (3, 4), whose average is (5 + 1) / 2 = 3;
-slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
-The goal is to find the starting position of a slice whose average is 
-minimal.
-
-Write a function:
-
-def solution(A)
-
-that, given a non-empty array A consisting of N integers, returns the 
-starting position of the slice with the minimal average. If there is 
-more than one slice with a minimal average, you should return the 
-smallest starting position of such a slice.
-
-For example, given array A such that:
-
-    A[0] = 4
-    A[1] = 2
-    A[2] = 2
-    A[3] = 5
-    A[4] = 1
-    A[5] = 5
-    A[6] = 8
-the function should return 1, as explained above.
-
-Assume that:
-
-N is an integer within the range [2..100,000];
-each element of array A is an integer within the range 
-[−10,000..10,000].
-Complexity:
-
-expected worst-case time complexity is O(N);
-
-expected worst-case space complexity is O(N)
-(not counting the storage required for input arguments).
-
-------------------------------------------
-
-use prefix sums to calculate average?
-
-    def prefix_sums(A):
-        n = len(A)
-        P = [0] * (n + 1)
-        for k in range(1, n + 1):
-            P[k] = P[k - 1] + A[k - 1]
-        return P
-
-    def count_total(P, x, y):
-        return P[y + 1] - P[x]
+def solution(A):
+    smalleststartingposition = 0
+    minimalavg = (A[0] + A[1]) / 2
+    
+    for i in range(2, len(A)):
+        avg2 = (A[i-1] + A[i]) / 2 
+        avg3 = (A[i-2] + A[i-1] + A[i]) / 3
         
+        if avg2 < minimalavg:
+            minimalavg = avg2
+            smalleststartingposition = i-1
+            
+        if avg3 < minimalavg:
+            minimalavg = avg3
+            smalleststartingposition = i-2
+            
+    return smalleststartingposition
