94b2d5ee3952ca3f645cda359b7fe56fecf64288
Marcus Plieninger <mwp2101@columbia.edu>
2018-06-21 23:16:26 -0400

    scored 100%

--- ch5_GenomicRangeQuery.py
+++ ch5_GenomicRangeQuery.py
@@ -1,17 +1,43 @@
-def solution(A):
-    smalleststartingposition = 0
-    minimalavg = (A[0] + A[1]) / 2
-    
-    for i in range(2, len(A)):
-        avg2 = (A[i-1] + A[i]) / 2 
-        avg3 = (A[i-2] + A[i-1] + A[i]) / 3
-        
-        if avg2 < minimalavg:
-            minimalavg = avg2
-            smalleststartingposition = i-1
+def solution(S, P, Q):
+    ones = []
+    twos = []
+    threes = []
+    fours = []
+    sumOnes = sumTwos = sumThrees = sumFours = 0
+
+    for nucleotide in S:
+        if nucleotide == "A":
+            sumOnes +=1
+        if nucleotide == "C":
+            sumTwos +=1
+        if nucleotide == "G":
+            sumThrees +=1
+        if nucleotide == "T":
+            sumFours +=1
+        ones.append(sumOnes)
+        twos.append(sumTwos)
+        threes.append(sumThrees)
+        fours.append(sumFours)
+
+    result = []
+    for i in range(len(P)):
+        if P[i] == 0:
+            A = ones[Q[i]]
+            C = twos[Q[i]]
+            G = threes[Q[i]]
+            T = fours[Q[i]]
+        else:
+            A = ones[Q[i]] - ones[P[i]-1]
+            C = twos[Q[i]] - twos[P[i]-1]
+            G = threes[Q[i]] - threes[P[i]-1]
+            T = fours[Q[i]] - fours[P[i]-1]
+        if A > 0:
+            result.append(1)    
+        elif C > 0:
+            result.append(2)
+        elif G > 0:
+            result.append(3)
+        else:
+            result.append(4)
             
-        if avg3 < minimalavg:
-            minimalavg = avg3
-            smalleststartingposition = i-2
-            
-    return smalleststartingposition
+    return result
