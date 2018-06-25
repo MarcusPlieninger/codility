6bac6652cca938d618618859e3fc084f55a22e87
MarcusPlieninger <mwp2101@columbia.edu>
2018-06-25 04:44:38 -0400

    space complexity comments, chose better variable names

--- ch6_MaxProductOfThree_mergesort.py
+++ ch6_MaxProductOfThree_mergesort.py
@@ -1,53 +1,44 @@
 '''
-The easy version:
+This version satisfies O(1) space complexity requirement.
 
 def solution(A):
     A = sorted(A)
     return max(A[0] * A[1] * A[-1], A[-1] * A[-2] * A[-3])
 
 '''
-
-
+'In this version, space complexity is bounded by O(N).'
 
 def solution(A):
 
-    def mergeSort(A):
-        n = len(A)
-        if n > 1:
-            # reduce input (ie, divide)
-            m = n // 2 
-            A1, A2 = A[:m], A[m:]
-            # create call stack by calling function on each half (ie, recursion)
-            mergeSort(A1)
-            mergeSort(A2)
-            # merge pieces back to original list in sorted order (ie, conquer)
-            merge(A1, A2, A)
-        return A
-
-    # merge A1 and A2 into A
-    def merge(A1, A2, A):
-        # keep track of current index in each list
-        iA1, iA2, iA = 0, 0, 0 # all start at front
-        n1, n2 = len(A1), len(A2)
-        # loop while A1 and A2 have more items
-        while iA1 < n1 and iA2 < n2:
-            if A1[iA1] < A2[iA2]:
-                A[iA] = A1[iA1]
-                iA1 += 1
+    def mergeSort(aList):)
+        if len(aList) > 1:
+            midPoint = len(aList) // 2 
+            leftHalf, rightHalf = A[:midPoint], A[midPoint:]
+           
+            mergeSort(lefHalf)
+            mergeSort(rightHalf)
+            
+        iLeftHalf = iRightHalf = iFinalListResult = 0
+        n1, n2 = len(lefHalf), len(rightHalf)
+        # loop while lefHalf and rightHalf have more items
+        while iLeftHalf < len(leftHalf) and len(rightHalf) < n2:
+            if lefHalf[iLeftHalf] < rightHalf[iRightHalf]:
+                A[iFinalListResult] = lefHalf[iLeftHalf]
+                iLeftHalf += 1
             else:
-                A[iA] = A2[iA2]
-                iA2 += 1
-            iA +=1
-        # copy any remaining items from A1
-        while iA1 < n1:
-            A[iA] = A1[iA1]
-            iA1 += 1
-            iA += 1
-        # copy any remaining items from A2
-        while iA2 < n2:
-            A[iA] = A2[iA2]
-            iA2 += 1
-            iA += 1
+                A[iFinalListResult] = rightHalf[iRightHalf]
+                iRightHalf += 1
+            iFinalListResult +=1
+        # copy any remaining items from lefHalf
+        while iLeftHalf < n1:
+            A[iFinalListResult] = lefHalf[iLeftHalf]
+            iLeftHalf += 1
+            iFinalListResult += 1
+        # copy any remaining items from rightHalf
+        while iRightHalf < n2:
+            A[iFinalListResult] = rightHalf[iRightHalf]
+            iRightHalf += 1
+            iFinalListResult += 1
 
     A = mergeSort(A)
     return max(A[0] * A[1] * A[-1], A[-1] * A[-2] * A[-3])
