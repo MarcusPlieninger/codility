0bcdc5a972019cc27c5b2e170c1b454e56a0815e
MarcusPlieninger <mwp2101@columbia.edu>
2018-06-26 19:47:37 -0400

    added a couple tests

---
+++ test_ch6_triangle.py
@@ -0,0 +1,54 @@
+'''
+Python
+
+1
+Task description
+An array A consisting of N integers is given. A triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N and:
+
+A[P] + A[Q] > A[R],
+A[Q] + A[R] > A[P],
+A[R] + A[P] > A[Q].
+For example, consider array A such that:
+
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+Triplet (0, 2, 4) is triangular.
+
+Write a function:
+
+def solution(A)
+
+that, given an array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and returns 0 otherwise.
+
+For example, given array A such that:
+
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+the function should return 1, as explained above. Given array A such that:
+
+  A[0] = 10    A[1] = 50    A[2] = 5
+  A[3] = 1
+the function should return 0.
+
+Assume that:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+Complexity:
+
+expected worst-case time complexity is O(N*log(N));
+expected worst-case space complexity is O(N) (not counting the storage required for input arguments).
+'''
+import unittest
+from ch6_Triangle import solution
+
+class example_tests(unittest.TestCase):
+    def test_example1(self):
+        A = [10, 2, 5, 1, 8, 20]
+        assertEquals(solution(A), 1)
+    def test_example2(self):
+        A = [10, 50, 5, 1]
+        assertEquals(solution(A), 0)
+
+if __name__ == 'main':
+    unittest.main()
