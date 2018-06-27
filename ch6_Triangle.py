fdb11970ab1349c354fa170ef35e9c3f7f9375b6
MarcusPlieninger <mwp2101@columbia.edu>
2018-06-27 04:46:26 -0400

    scored 100%

--- ch6_Triangle.py
+++ ch6_Triangle.py
@@ -1,4 +1,28 @@
-# variation on merge sort
-# divide list in two halves recursively 
-# base case is 2 numbers
-# save the pair if left number is less than right number
+'''
+because A is sorted, the following obtains:
+A[i] <= A[i+1] <= A[i+2]
+A[i] <= A[i+2]
+
+therefore:
+A[i] < A[i+1] + A[i+2]
+A[i+1] < A[i] + A[i+2]
+
+Triangular is defined by:
+0 <= A[i] < A[i+1] < A[i+2] < N (satisfied by above)
+A[i] + A[i+1] > A[i+2] this criterion must be checked
+A[i+1] + A[i+2] > A[i] (satisfied by above)
+A[i+2] + A[i] > A[i+1] (satisfied by above)
+
+'''
+
+def solution(A):
+    if len(A) < 3:
+         return 0
+    
+    A = sorted(A)
+    
+    for i in range(0, len(A)-2):
+        if A[i] + A[i+1] > A[i+2]:
+            return 1
+            
+    return 0
